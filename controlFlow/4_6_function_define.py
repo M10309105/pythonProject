9293725b4be28acd7bda040a9313c866e5bd8c85
Francis Lin <a1a2a3asdzxc@gmail.com>
2017-09-24 16:38:52 +0800

    4_6_function_define.pyi: add Hanoi Tower method testing

--- controlFlow/4_6_function_define.py
+++ controlFlow/4_6_function_define.py
@@ -1,29 +1,61 @@
 # 4.6 function define
+#Hanoi Tower
 
 
-def fin(n):
+def fin(n,reminder="finished!"):
 	a,b = 0,1
 	while a < n :
 		print(a,end=" ")
 		a,b=b,a+b
-	print()
+	print(reminder)
 
 fin(5000)
 
-print("document example")
 
-def ask_ok(prompt, retries=4, reminder='Please try again!'):
-    while True:
-        ok = input(prompt)
-        if ok in ('y', 'ye', 'yes'):
-            return "Y"
-        if ok in ('n', 'no', 'nop', 'nope'):
-            return "N"
-        retries = retries - 1
-        if retries < 0:
-            raise ValueError('invalid user response')
-        print(reminder)
+def tower(index, _from, temp, to):
+	if index == 1:
+		print("put index ",index," from ",_from," to ",to)
+	else:
+		tower(index-1,_from,to,temp)
+		print("put index ",index," from ",_from," to ",to)
+		tower(index-1,temp,_from,to)
+
+	
+tower(3,"A","B","C")
+print("""
+
+""")
+c=0
+def towerByCount(index, _from, temp, to):
+	global c
+	if index == 1:
+		c+=1
+		print("Step(",c,")put index ",index," from ",_from," to ",to)
+	else:
+		towerByCount(index-1,_from,to,temp)
+		c+=1
+		print("Step(",c,")put1 index ",index," from ",_from," to ",to)
+		towerByCount(index-1,temp,_from,to)
 		
-print(ask_ok('Do you really want to quit?'))
-print(ask_ok('OK to overwrite the file?', 2))
-print(ask_ok('OK to overwrite the file?', 2, 'Come on, only yes or no!'))
+
+towerByCount(5,"A","B","C")
+print("Total step in 5 layers of Hanoi Tower:",c)
+
+print()
+"""
+When a final formal parameter of the form **name is present, it receives a dictionary (see Mapping Types — dict)
+ containing all keyword arguments except for those corresponding to a formal parameter. 
+ This may be combined with a formal parameter of the form *name (described in the next subsection) 
+ which receives a tuple containing the positional arguments beyond the formal parameter list. (*name must occur before **name.)
+"""
+def moreTest(*name,**dict):
+	for arg in name:
+		print(arg)
+	print()
+	print("="*50)
+	
+	keys = sorted(dict.keys())
+	for k in keys:
+		print(k,":",dict[k])
+		
+moreTest("this is name1","name2","name3","nameN",Name="Tom",Age=50,gender="male",phone="0987654321")
