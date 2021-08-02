8f51c3f6fe28d623b9131be1e883520309ee627a
francis <M10309105@mail.ntust.edu.tw>
2021-08-02 22:09:14 +0800

    lesson 8 to 8.2

---
+++ lesson/8/8.1.py
@@ -0,0 +1,11 @@
+#/usr/bin/python3
+#
+#Syntax Error
+print("=" * 100)
+print("8.1 Syntax Error")
+print("=" * 100)
+#if 1>0 print("hello world")
+
+# if 1>0 print("hello world")
+#            ^
+# SyntaxError: invalid syntax    
---
+++ lesson/8/8.2.py
@@ -0,0 +1,12 @@
+#/usr/bin/python3
+#
+#Exception
+print("=" * 100)
+print("8.2 Exceptionr")
+print("=" * 100)
+
+a = 1/0
+print(a)
+
+#ZeroDivisionError: division by zero
+#ZeroDivisionError、NameError 和 TypeError
