fa7f632f84c659cf2423aabb1ac7bfde92e9ba2e
francis <M10309105@mail.ntust.edu.tw>
2021-08-06 23:50:57 +0800

    lesson 9.1

---
+++ lesson/9/9.1.py
@@ -0,0 +1,10 @@
+#/usr/bin/python3
+#
+#Class
+print("=" * 100)
+print("9.1 ")
+print("=" * 100)
+"""
+A Word About Names and Objects
+
+"""
---
+++ lesson/9/9.2.py
@@ -0,0 +1,6 @@
+#/usr/bin/python3
+#
+#Class
+print("=" * 100)
+print("9.2 Python Scopes and Namespaces¶")
+print("=" * 100)
