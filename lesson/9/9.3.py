00d25a5916c149d03f043b2fbb58ae0b75aa819c
francis <M10309105@mail.ntust.edu.tw>
2021-08-09 23:40:30 +0800

    lesson 9.3.3 to 9.3.5

--- lesson/9/9.3.py
+++ lesson/9/9.3.py
@@ -30,10 +30,57 @@ class Car:
     def __init__(self,type, color):
         self.type = type
         self.color = color
+        self.num = 0
 
     def foo(self):
-        print("This is Car.foo")
+        print("This is Car.foo, this car is",self.type,self.color,"car")
 
 print(Car.__doc__)
 car1 = Car("Big","red")
 print(car1.type, car1.color)
+
+print("=" * 100)
+print("9.3.3 object")
+print("=" * 100)
+
+car1.num = 1
+for i in range(10):
+    car1.num = car1.num * 2
+print(car1.num)
+del car1.num
+
+print("=" * 100)
+print("9.3.4 method object")
+print("=" * 100)
+car1 = Car("Big","red")
+car1.foo()
+
+
+print("=" * 100)
+print("9.3.5 Class and Instance Variables")
+print("=" * 100)
+
+car1 = Car("Big","red")
+car2 = Car("small","green")
+print(car1.type, car1.color)
+print(car2.type, car2.color)
+
+class TaxiCar(Car):
+    #this is class variable
+    passenger = []
+    def __init__(self,type, color):
+        self.type = type
+        self.color = color
+        #self obj
+        self.passenger1 = []
+    
+    def getInto(self,passenger):
+        self.passenger.append(passenger)
+
+taxi = TaxiCar("big","yellow")
+taxi.getInto("Tom")
+taxi.getInto("Peter")
+print(taxi.passenger)
+taxi1 = TaxiCar("small","yellow")
+taxi.getInto("Zoe")
+print(taxi.passenger)
