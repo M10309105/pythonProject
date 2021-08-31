2e39bbf3b2e441a82f39fbf864434060d31b9b37
francis <M10309105@mail.ntust.edu.tw>
2021-08-31 23:42:02 +0800

    lesson 11.1

---
+++ lesson/11/11.1.py
@@ -0,0 +1,11 @@
+#/usr/bin/python3
+#
+#Class
+print("=" * 100)
+print("11.1 Output Formatting")
+print("=" * 100)
+
+#reprlib
+
+import reprlib
+print(reprlib.repr(set('supercalifragilisticexpialidocious')))
